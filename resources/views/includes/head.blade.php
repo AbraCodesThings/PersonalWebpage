b41cf2f0a1e0a8e73fc51f6b67feefde94b12c2e
Abraham Díaz <abradiazmediavilla@gmail.com>
2023-06-12 13:30:19 +0200

    Styles, draggable windows behavior (some of it), added a font

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,4 +2,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>My webpage</title>
 <link rel="stylesheet" href="{{asset('style.css')}}">
-<link rel="stylesheet" href="{{asset('bootstrap/bootstrap.min.css')}}">
+<!-- <link rel="stylesheet" href="{{asset('bootstrap/bootstrap.min.css')}}"> -->
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,10 +1,29 @@
 @extends('master')
 @section('content')
-    <h1>DOES THIS WORK?</h1>
-    <div></div>
+    <div class="desktop" id="desktop">
+        <!-- Here be windows -->
+    </div>
+    <div class="start_bar">
+        <div class="start_button">
+            <!--  -->
+            <span>Start</span>
+        </div>
+        <div class="start_minimized">
+            <!--  -->
+        </div>
+        <div class="start_tools">
+            <div class="start_tools_icons">
+                <!--  -->
+            </div>
+            <div class="start_tools_clock">
+                <!--  -->
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
+    <script src="scripts/windows.js"></script>
     <script>
         
     </script>
